1ea42f24ce63cfe33f59afacdcaf3c079835c655
bagels725 <130070709+bagels725@users.noreply.github.com>
2023-05-23 11:15:43 -0400

    Update blockchain.ts

--- blockchain.ts
+++ blockchain.ts
@@ -1,5 +1,5 @@
-# blockchain.ts
-# Generic blockchain program that Nebu uses.
+#blockchain.ts
+#Generic blockchain program that Nebu uses.
 class Block {
 
   public index: number;
@@ -17,16 +17,16 @@ class Block {
   }
 }
 
-# Calculate block hash
+#Calculate block hash
 const calculateHash = (index: number, hash: string, previousHash: string, timestamp: number, data: string): string =>
   CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
-# Genesis block
+#Genesis block
 const genesisBlock: Block = new Block(
   0, '816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7', null, 1465154705, 'So long, and thanks for all the fish.'
 );
 
-# Generate next block
+#Generate next block
 const generateNextBlock = (blockData: string) => {
   const previousBlock: Block = getLatestBlock();
   const nextIndex: number = previousBlock.index + 1;
@@ -36,10 +36,10 @@ const generateNextBlock = (blockData: string) => {
   return newBlock;
 };
 
-# Stores in JS memory array. not permanent!!!
+#Stores in JS memory array. not permanent!!!
 const blockchain: Block[] = [genesisBlock];
 
-# Validates blocks
+#Validates blocks
 const isValidNewBlock = (newBlock: Block, previousBlock: Block) => {
   if (previousBlock.index + 1 !== newBlock.index) {
     console.log('OOPS! Invalid index.');
@@ -55,7 +55,7 @@ const isValidNewBlock = (newBlock: Block, previousBlock: Block) => {
   return true;
 };
 
-# Validate block structure to check for malformed content.
+#Validate block structure to check for malformed content.
 const isValidBlockStructure = (block: Block): boolean => {
   return typeof block.index === 'number'
       && typeof block.hash === 'string'
@@ -64,7 +64,7 @@ const isValidBlockStructure = (block: Block): boolean => {
       && typeof block.data === 'string';
 };
 
-# Validates blockchain
+#Validates blockchain
 const isValidChain = (blockchainToValidate: Block[]): boolean => {
   const isValidGenesis = (block: Block): boolean => {
     return JSON.stringify(block) === JSON.stringify(genesisBlock);
@@ -83,7 +83,7 @@ const isValidChain = (blockchainToValidate: Block[]): boolean => {
   return true;
 };
 
-# Blockchain conflict resolver. Chooses longest chain.
+#Blockchain conflict resolver. Chooses longest chain.
 const replaceChain = (newBlocks: Block[]) => {                                                  
   if (isValidChain(newBlocks) && newBlocks.length > getBlockchain().length) {
     console.log('Recieved blockchain is valid. Replacing current blockchain with recieved blockchain.');
@@ -94,7 +94,7 @@ const replaceChain = (newBlocks: Block[]) => {
   }   
 };
 
-# Control node
+#Control node
 const initHttpServer = ( myHttpPort: number ) => {
     const app = express();
     app.use(bodyParser.json());
@@ -119,4 +119,4 @@ const initHttpServer = ( myHttpPort: number ) => {
     });
 };
     
-# Get all blocks from node in console: curl http://localhost:3001/blocks
+#Get all blocks from node in console: curl http://localhost:3001/blocks
